2e27c1c6a5733b8bf589002016a5c7396a70dbe8
deniskorbakov <korbakovd@gmail.com>
2024-10-07 23:09:16 +0300

    fix: update all logic worked with files

--- file.go
+++ file.go
@@ -12,10 +12,6 @@ type File struct {
 	Name string
 }
 
-func GetFullPath(filePath string, fileName string) string {
-	return filepath.Join(filePath, fileName)
-}
-
 func (fl *File) IsExistFile() bool {
 	_, err := fl.ReadFile()
 	if err != nil {
@@ -25,8 +21,12 @@ func (fl *File) IsExistFile() bool {
 	return true
 }
 
+func (fl *File) fullPath() string {
+	return filepath.Join(fl.Path, fl.Name)
+}
+
 func (fl *File) CreateFile() error {
-	file := GetFullPath(fl.Path, fl.Name)
+	file := fl.fullPath()
 
 	if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
 		dir := filepath.Dir(file)
@@ -50,7 +50,7 @@ func (fl *File) CreateFile() error {
 }
 
 func (fl *File) ReadFile() (string, error) {
-	file := GetFullPath(fl.Path, fl.Name)
+	file := fl.fullPath()
 
 	f, err := os.Open(file)
 	if err != nil {
@@ -69,7 +69,7 @@ func (fl *File) ReadFile() (string, error) {
 }
 
 func (fl *File) WriteFile(rowData []byte) error {
-	file := GetFullPath(fl.Path, fl.Name)
+	file := fl.fullPath()
 
 	err := ioutil.WriteFile(file, rowData, 0)
 	if err != nil {
