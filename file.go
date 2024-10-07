1e609468ea681f9e05d3ea0c42ab1aa7f6641626
deniskorbakov <korbakovd@gmail.com>
2024-10-07 22:20:52 +0300

    feat: add struct for methods

--- file.go
+++ file.go
@@ -2,38 +2,65 @@ package file
 
 import (
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
-func CreateFile(filePath string) {
-	if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
-		dir := filepath.Dir(filePath)
+type File struct {
+	path string
+	name string
+}
+
+func GetFullPath(filePath string, fileName string) string {
+	return filepath.Join(filePath, fileName)
+}
+
+func (fl *File) IsExistFile() bool {
+	_, err := fl.ReadFile()
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (fl *File) CreateFile() error {
+	file := GetFullPath(fl.path, fl.name)
+
+	if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
+		dir := filepath.Dir(file)
 
 		err = os.MkdirAll(dir, os.ModePerm)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
-		createdFile, err := os.Create(filePath)
+		createdFile, err := os.Create(file)
 		if err != nil {
-			fmt.Println(err)
+			return err
 		}
 
-		defer createdFile.Close()
+		defer func(createdFile *os.File) {
+			err = createdFile.Close()
+		}(createdFile)
 	}
+
+	return nil
 }
 
-func ReadFile(filePath string) (string, error) {
-	f, err := os.Open(filePath)
+func (fl *File) ReadFile() (string, error) {
+	file := GetFullPath(fl.path, fl.name)
+
+	f, err := os.Open(file)
 	if err != nil {
 		return "", err
 	}
-	defer f.Close()
+	defer func(f *os.File) {
+		err = f.Close()
+	}(f)
 
-	fContent, err := ioutil.ReadFile(filePath)
+	fContent, err := ioutil.ReadFile(file)
 	if err != nil {
 		return "", err
 	}
@@ -41,10 +68,12 @@ func ReadFile(filePath string) (string, error) {
 	return string(fContent), nil
 }
 
-func WriteFile(path string, rowData []byte) error {
-	err := ioutil.WriteFile(path, rowData, 0)
+func (fl *File) WriteFile(rowData []byte) error {
+	file := GetFullPath(fl.path, fl.name)
+
+	err := ioutil.WriteFile(file, rowData, 0)
 	if err != nil {
-		return errors.New("error when writing to a file")
+		return err
 	}
 
 	return nil
