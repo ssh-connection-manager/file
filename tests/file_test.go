b4bb1f59f5464c2dafeb00ae660018585280eb4c
deniskorbakov <korbakovd@gmail.com>
2024-11-28 23:03:15 +0300

    feat: create tests

--- tests/file_test.go
+++ tests/file_test.go
@@ -1,19 +1,105 @@
 package tests
 
 import (
+	"fmt"
 	fl "github.com/ssh-connection-manager/file"
+	"math/rand"
+	"os/user"
 	"testing"
 )
 
+func randomString(count int) string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	randomStr := make([]rune, count)
+	for i := range randomStr {
+		randomStr[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(randomStr)
+}
+
+func getHomeDir() string {
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return usr.HomeDir + "/test/"
+}
+
 func TestCreateFile(t *testing.T) {
-	file := fl.File{Name: "test1", Path: "/home/deniskorbakov"}
+	homeDir := getHomeDir()
+	randomStr := randomString(5)
+
+	file := fl.File{Name: randomStr, Path: homeDir}
 	err := file.CreateFile()
 	if err != nil {
 		t.Fatal("Error creating file")
 	}
 
+	if !file.IsExistFile() {
+		t.Fatal("File dont created")
+	}
 }
 
-func TestWriteToFile(t *testing.T) {}
+func TestWriteToFile(t *testing.T) {
+	randomStr := randomString(5) + ".txt"
+	homeDir := getHomeDir()
 
-func TestReadFile(t *testing.T) {}
+	file := fl.File{Name: randomStr, Path: homeDir}
+	err := file.CreateFile()
+	if err != nil {
+		t.Fatal("Error creating file")
+	}
+
+	randomText := randomString(100)
+	err = file.WriteFile([]byte(randomText))
+	if err != nil {
+		t.Fatal("Error write to file")
+	}
+
+	fileText, err := file.ReadFile()
+	if err != nil {
+		t.Fatal("Error read file")
+	}
+
+	if fileText != randomText {
+		t.Fatal("Error random text != text from file")
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	files := [7]fl.File{
+		{Name: randomString(5) + ".json", Path: getHomeDir()},
+		{Name: randomString(5) + ".txt", Path: getHomeDir()},
+		{Name: randomString(5) + ".PNG", Path: getHomeDir()},
+		{Name: randomString(5) + ".PDF", Path: getHomeDir()},
+		{Name: randomString(5) + ".PDF", Path: getHomeDir()},
+		{Name: randomString(5) + ".DOC", Path: getHomeDir()},
+		{Name: randomString(5), Path: getHomeDir()},
+	}
+
+	for _, file := range files {
+		err := file.CreateFile()
+		if err != nil {
+			t.Fatal("Error creating file")
+		}
+
+		randomText := randomString(100)
+
+		err = file.WriteFile([]byte(randomText))
+		if err != nil {
+			t.Fatalf("Error write to file %s", file)
+		}
+
+		fileText, err := file.ReadFile()
+		if err != nil {
+			t.Fatal("Error read file")
+		}
+
+		if fileText != randomText {
+			t.Fatalf("Error random text != text from file - is file %s", file.Path+file.Name)
+		}
+	}
+}
