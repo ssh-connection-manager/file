bdb659ea0f23e81dfd26f2186709ba651fc5a787
deniskorbakov <korbakovd@gmail.com>
2024-12-12 22:38:14 +0300

    fix: update logic write to file

--- file.go
+++ file.go
@@ -65,7 +65,7 @@ func (fl *File) ReadFile() (string, error) {
 func (fl *File) WriteFile(rowData []byte) error {
 	file := fl.fullPath()
 
-	f, err := os.OpenFile(file, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	f, err := os.OpenFile(file, os.O_WRONLY, 0666)
 	if err != nil {
 		return err
 	}
